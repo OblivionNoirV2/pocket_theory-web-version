9b141072f004845308dab8530ca0ae8eb56f80a0
OblivionNoirV2 <donahueb85@gmail.com>
2023-04-12 20:01:20 -0500

    basic webpage

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as calculator from './calculate';
+//simple header with 66% width design
+const MainBody = () => {
+  return (
+    <main className='bg-slate-200 h-screen w-1/2 flex flex-col m-auto'>
+      <div className="bg-slate-400 w-full z-2 py-2 max-h-60 text-center 
+      text-4xl">
+        Quick Music Theory Reference
+      </div>
+      <div className="flex-grow flex justify-center items-center">
+        <div className="flex space-x-4">
+          <select className="bg-white text-black py-2 px-4 rounded">
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+          <select className="bg-white text-black py-2 px-4 rounded">
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+        </div>
+      </div>
+    </main>
+  );
+};
 
+const Calculator = () => {
+  //calls from the other file
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header text-xl">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>hello</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainBody />
   );
 }
 
---
+++ my-app/src/calculate.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+namespace Calculations {
+    class InputGathering {
+
+    }
+}
+
+export default Calculations;
